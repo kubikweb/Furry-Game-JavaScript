ef92152a397f240208711ef65efe721e37568dc2
Kamila Kubik <kotem84@gmail.com>
2025-11-27 16:26:19 +0100

    Webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
     entry: "./js/app.js",
     output: {
-    filename: "out.js"
+        filename: "app.js",
+        path: path.resolve(__dirname, 'public')
     },
-    watch: true
-}
+    watch: true,
+    mode: 'development',
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'index.html', to: 'index.html' },
+                { from: 'css', to: 'css' },
+                { from: 'images/furry2 (1).png', to: 'images/furry2 (1).png' },
+                { from: 'images/bitcoin.png', to: 'images/bitcoin.png' }
+            ]
+        })
+    ]
+};
